0aef657f92a07483b3260287c4f195c24869da95
tu-ngo <tu.ngo@gooddata.com>
2021-11-10 15:23:47 +0700

    Create Kafka Producer and Consumer

---
+++ good-kafka/src/main/java/com/gooddata/rubik/hackathon/goodkafka/GoodKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gooddata.rubik.hackathon.goodkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodKafkaApplication.class, args);
+    }
+
+}
